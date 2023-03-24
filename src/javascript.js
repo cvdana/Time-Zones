dd558069ba2e2fd1f7460fd33b4b4efba9298f9f
cvdana <cvdana99@gmail.com>
2023-03-24 21:53:34 +0100

    Updated JS with timezones

--- src/javascript.js
+++ src/javascript.js
@@ -1,17 +1,25 @@
 function showSelectedZone(event) {
   if (event.target.value.length > 0) {
     if (event.target.value === `paris`) {
-      let currentDateElement = document.querySelector(`#paris`);
+      let currentDateElementParis = document.querySelector(`#paris`);
       let currentDate = moment()
         .tz(`Europe/Paris`)
-        .format(`dddd, MMMM  D, YYYY h:mmA`);
+        .format(`dddd, MMMM D, YYYY h:mm A`);
       alert(`It is currently ${currentDate} in Paris/Europe`);
     }
     if (event.target.value === `tokyo`) {
-      alert(`nihao`);
+      let currentDateElementTokyo = document.querySelector(`#tokyo`);
+      let currentDate = moment()
+        .tz(`Asia/Tokyo`)
+        .format(`dddd, MMMM D, YYYY h:mm A`);
+      alert(`It is currently ${currentDate} in Tokyo/Asia`);
     }
     if (event.target.value === `sydney`) {
-      alert(`lalal`);
+      let currentDateElementSydney = document.querySelector(`#sydney`);
+      let currentDate = moment()
+        .tz(`Australia/Sydney`)
+        .format(`dddd, MMMM D, YYYY h:mm A`);
+      alert(`It is currently ${currentDate} in Sydney/Australia`);
     }
   }
 }
