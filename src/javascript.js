de63bf66172dcb2261107cf756faea469d1dd262
cvdana <cvdana99@gmail.com>
2023-03-27 11:25:53 +0200

    Improved code in JS

--- src/javascript.js
+++ src/javascript.js
@@ -1,26 +1,9 @@
 function showSelectedZone(event) {
   if (event.target.value.length > 0) {
-    if (event.target.value === `paris`) {
-      let currentDateElementParis = document.querySelector(`#paris`);
-      let currentDate = moment()
-        .tz(`Europe/Paris`)
-        .format(`dddd, MMMM D, YYYY h:mm A`);
-      alert(`It is currently ${currentDate} in Paris/Europe`);
-    }
-    if (event.target.value === `tokyo`) {
-      let currentDateElementTokyo = document.querySelector(`#tokyo`);
-      let currentDate = moment()
-        .tz(`Asia/Tokyo`)
-        .format(`dddd, MMMM D, YYYY h:mm A`);
-      alert(`It is currently ${currentDate} in Tokyo/Asia`);
-    }
-    if (event.target.value === `sydney`) {
-      let currentDateElementSydney = document.querySelector(`#sydney`);
-      let currentDate = moment()
-        .tz(`Australia/Sydney`)
-        .format(`dddd, MMMM D, YYYY h:mm A`);
-      alert(`It is currently ${currentDate} in Sydney/Australia`);
-    }
+    let currentDate = moment()
+      .tz(event.target.value)
+      .format(`dddd, MMMM D, YYYY h:mm A`);
+    alert(`It is currently ${currentDate} in ${event.target.value}`);
   }
 }
 let timeZoneSelect = document.querySelector(`#time-zones`);
