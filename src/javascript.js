560261729561901979c0e17a1b86fb5d8494fba5
cvdana <cvdana99@gmail.com>
2023-03-30 10:19:51 +0200

    Added analog clock in html and JS

--- src/javascript.js
+++ src/javascript.js
@@ -58,3 +58,17 @@ function updateCity(event) {
 }
 let citiesSelectElement = document.querySelector(`#time-zones`);
 citiesSelectElement.addEventListener(`change`, updateCity);
+
+setInterval(() => {
+  d = new Date();
+  hr = d.getHours();
+  min = d.getMinutes();
+  sec = d.getSeconds();
+  hr_rotation = 30 * hr + min / 2;
+  min_rotation = 6 * min;
+  sec_rotation = 6 * sec;
+
+  hour.style.transform = `rotate(${hr_rotation}deg)`;
+  minute.style.transform = `rotate(${min_rotation}deg)`;
+  second.style.transform = `rotate(${sec_rotation}deg)`;
+}, 1000);
